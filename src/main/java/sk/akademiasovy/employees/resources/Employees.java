00baa8c1e3c0eac31cd60602af14bcd138349692
Andrej <andrej.byra@centrum.sk>
2018-02-16 19:16:54 +0100

    Initial commit

---
+++ src/main/java/sk/akademiasovy/employees/resources/Employees.java
@@ -0,0 +1,24 @@
+package sk.akademiasovy.employees.resources;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import sk.akademiasovy.employees.db.MySQL;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/employees")
+public class Employees {
+    @POST
+    @Path("/salary")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getPopulation(Emp emplo)
+    {
+        String salary = new MySQL().getEmp(emplo.name);
+        return salary;
+    }
+    public static class Emp
+    {
+        @JsonProperty("name")
+        public String name ;
+    }
+}
